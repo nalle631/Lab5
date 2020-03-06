0614ad46158e03c5dc6aacee3e97fa3be4444463
oskar-dah <61740163+oskar-dah@users.noreply.github.com>
2020-03-06 13:13:15 +0100

    Update State.java

--- State.java
+++ State.java
@@ -1,29 +1,9 @@
 import java.util.Observable;
 
-/**
- * @author Lana Abdulla, lanabd-9
- * Klassen State håller på statusen på simuleringen , varje förändring
- * som sker och när view ändras och uppdateras
- */
-
-
 public class State extends Observable {
-	int CustomInStore = 0;//customers i butiken just nu
-	int totCustomOpen = 0; //totalt för dagen
-	int payedCustom = 0;
-	int missedCustom = 0;
-	
-	double totTimeInQueue = 0;
-	int lengthOfQueue = 0;
-	double totTimeCheckout = 0;
-	double freeTimeCheckout = 0;
-	
-	
-	
-	public final int maxCustomInStore;
-	public int maxCheckouts;
-	public double closingTime;
-	//public FIFO checkoutQueue;
+	private boolean activeSim;
+	private long timePassed;
+	private Event currentEvent;
 	
 	/**
 	 * Constructor
@@ -32,26 +12,45 @@ public class State extends Observable {
 	 * @param closingTime
 	 * @param freeTimeCheckout
 	 */
-	public State(int maxCustomInStore, int maxCheckouts, double closingTime, double freeTimeCheckout) {
-		this.maxCustomInStore = maxCustomInStore;
-		this.maxCheckouts = maxCheckouts;
-		this.closingTime = closingTime;
-		this.freeTimeCheckout = freeTimeCheckout;
-		//checkoutQueue = new FIFO (this);//Skapa FIFO klass?	  
+	public State() {
+		
+		this.activeSim = true;
+		  
 	}
 	
 	
-	private boolean activeSim;
-	private long timePassed;
+	
 	
 	
 	public long getTimePassed() {
 		return this.timePassed;
 	}
-	
-	public boolean stop() {
-		activeSim = false;
+	public boolean getActiveSim(){
 		return activeSim;
+	}
+	
+	
+	public void stop() {
+		activeSim = false;
+		
+	}
+
+
+
+
+
+	public Event getCurrentEvent() {
+		return currentEvent;
+	}
+
+
+
+
+
+	public void setCurrentEvent(Event currentEvent) {
+		this.currentEvent = currentEvent;
+		setChanged();
+		notifyObservers();
 	}
 	
 	//public State() {//returna tillståndet just nu?
